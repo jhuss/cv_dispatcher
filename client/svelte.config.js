75b0e2937f9ff234e05e30127fd0b901ecfed530
Jesus Jerez <dev@jerez.link>
2022-06-27 12:29:27 -0300

    minify css

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -1,14 +1,20 @@
 import sveltePreprocess from 'svelte-preprocess'
+import postcssMinify from 'postcss-minify'
 
 export default {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
-  preprocess: sveltePreprocess(
-    {
+  preprocess: [
+    sveltePreprocess({
       includePaths: [
         'node_modules',
         'src'
-      ]
-    }
-  )
+      ],
+      postcss: {
+        plugins: [
+          postcssMinify
+        ]
+      }
+    })
+  ]
 }
